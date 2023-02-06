bd687a59c9cb14fa95637aa4cf5f866f7102c29a
Konstantin <apnkel@gmail.com>
2023-02-06 15:06:29 +0300

    Fixed crashes and added table view

--- lib/disi_tools_web/controllers/password_generator.ex
+++ lib/disi_tools_web/controllers/password_generator.ex
@@ -25,23 +25,64 @@ defmodule DisiTools.PasswordGenerator do
 
   def index(conn, params) do
     case params do
+      %{"count" => count, "table" => table} ->
+        if table == "true" do
+          try do
+            cnt = String.to_integer(count)
+              if cnt <= 16 do
+                clepwd = fn ->
+                  {:ok, pwd} = genpassword()
+                  [pwd]
+                end
+
+                data = for _ <- 1..cnt, do: clepwd.()
+
+                header = ["Passwords"]
+                IO.inspect(data)
+                text(conn,TableRex.quick_render!(data, header))
+              else
+                conn
+                |> put_status(503)
+                |> text("Wrong count of passwords")
+              end
+            rescue
+              ArgumentError ->
+                conn
+                |> put_status(503)
+                |> text("Wrong count of passwords")
+          end
+        end
+
       %{"count" => count, "length" => length} ->
         try do
           cnt = String.to_integer(count)
-
-          clepwd = fn l ->
-            {:ok, pwd} = genpassword(l)
-            pwd
+          if cnt <= 16 do
+            clepwd = fn l ->
+              pwd = genpassword(l)
+              case pwd do
+                {:ok, pass} -> pass
+                {:error, _} -> "WRONG"
+              end
+            end
+            data = for _ <- 1..cnt, do: clepwd.(length)
+            if data |> Enum.at(0) != "WRONG" do
+              json(conn, %{passwords: data})
+            else
+              conn
+              |> put_status(503)
+              |> json(%{text: "Wrong password length."})
+            end
+          else
+            conn
+            |> put_status(503)
+            |> json(%{text: "Maximum count is 8"})
           end
-
-          data = for _ <- 1..cnt, do: clepwd.(length)
-
-          json(conn, %{passwords: data})
         rescue
           ArgumentError ->
-            {:error, "Wrong type."}
+            conn
+            |> put_status(503)
+            |> json(%{text: "Wrong type."})
         end
-
       %{"count" => count} ->
         try do
           cnt = String.to_integer(count)
@@ -56,7 +97,9 @@ defmodule DisiTools.PasswordGenerator do
           json(conn, %{passwords: data})
         rescue
           ArgumentError ->
-            {:error, "Wrong type."}
+            conn
+            |> put_status(503)
+            |> json(%{text: "Wrong type."})
         end
 
       %{"length" => length} ->
@@ -67,7 +110,6 @@ defmodule DisiTools.PasswordGenerator do
           end
 
         json(conn, %{password: text})
-
       _ ->
         {:ok, password} = genpassword()
         json(conn, %{password: password})
--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule DisiTools.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:table_rex, "~> 3.1.1"}
     ]
   end
 
